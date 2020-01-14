cbb02a90d62abdeef2e498c483a88041ac02e97d
lonelygoogle <lonelygoogle@qq.com>
2020-01-14 22:43:24 +0800

    add Header

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/pages/home/home'
+import Home from '@/pages/home/Home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: home
+      name: 'Home',
+      component: Home
     }
   ]
 })
